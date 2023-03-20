0c29b6d1bb6e95a6c76782fc7b0d5d4a7d26d41b
dzikkk <karol.olchowy@gmail.com>
2023-03-20 14:34:14 +0100

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders point summary header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/Points Summary/i);
   expect(linkElement).toBeInTheDocument();
 });
